bbcc604be6bae3a7988b3d0bf3d25b61b08a588c
kororu-lab <chch0926@yonsei.ac.kr>
2023-11-27 15:20:16 +0000

    Regular Update

--- data/auto.sh
+++ data/auto.sh
@@ -13,32 +13,74 @@ submissions_files=($(ls "$HDD_DIR_SUBMISSIONS"/*.zst))
 process_files() {
     local comment_file="$1"
     local submission_file="$2"
-    
+    local comment_basename=$(basename "$comment_file")
+    local submission_basename=$(basename "$submission_file")
+
+    echo "Processing $comment_basename and $submission_basename..."
+
     # Copy files to SSD for processing
-    echo "Copying $comment_file to SSD..."
+    echo "Copying $comment_file to $SSD_DIR/compressed/"
     cp "$comment_file" "$SSD_DIR/compressed/"
     
-    echo "Copying $submission_file to SSD..."
+    echo "Copying $submission_file to $SSD_DIR/compressed/"
     cp "$submission_file" "$SSD_DIR/compressed/"
     
-    # Print the files in the compressed directory for debugging
-    echo "Files in the compressed directory:"
-    ls "$SSD_DIR/compressed/"
+    # Execute the processing script for specific files with full filenames
+    echo "Running process.sh for $comment_basename and $submission_basename"   
+    bash "$SSD_DIR/process.sh" "$SSD_DIR" "$comment_basename" "$submission_basename"
+
+    # Corrected file basename to exclude '.zst' extension for processed file check
+    local processed_comment_basename=$(basename "$comment_file" .zst)
+    local processed_submission_basename=$(basename "$submission_file" .zst)
+
+    # Check if processed files exist
+    echo "Checking for processed files..."
+    if [[ -f "$SSD_DIR/processed/${processed_comment_basename}.csv" ]]; then
+        echo "Moving ${processed_comment_basename}.csv to $HDD_DIR_PROCESSED"
+        mv "$SSD_DIR/processed/${processed_comment_basename}.csv" "$HDD_DIR_PROCESSED/"
+    else
+        echo "Processed file ${processed_comment_basename}.csv not found."
+    fi
+
+    if [[ -f "$SSD_DIR/processed/${processed_submission_basename}.csv" ]]; then
+        echo "Moving ${processed_submission_basename}.csv to $HDD_DIR_PROCESSED"
+        mv "$SSD_DIR/processed/${processed_submission_basename}.csv" "$HDD_DIR_PROCESSED/"
+    else
+        echo "Processed file ${processed_submission_basename}.csv not found."
+    fi
     
-    # Execute the processing script for specific files
-    echo "Processing files..."
-    bash "$SSD_DIR/process.sh" "$(basename "$comment_file")" "$(basename "$submission_file")"
-    
-    # Move processed files to the HDD processed directory
-    echo "Moving processed files to HDD..."
-    sudo mv "$SSD_DIR/processed/"*.csv "$HDD_DIR_PROCESSED/"
-    
-    # Clean up the SSD compressed and released directories
-    echo "Cleaning up SSD directory..."
-    rm -fv "$SSD_DIR/compressed/"*
-    rm -fv "$SSD_DIR/released/"*
-    
-    echo "Processed files: $(basename "$comment_file") and $(basename "$submission_file")"
+    # Cleanup section
+    echo "Cleaning up files for $comment_basename and $submission_basename..."
+    local compressed_comment_path="$SSD_DIR/compressed/$comment_basename"
+    local compressed_submission_path="$SSD_DIR/compressed/$submission_basename"
+    local released_comment_path="$SSD_DIR/released/$(basename "$comment_basename" .zst).ndjson"
+    local released_submission_path="$SSD_DIR/released/$(basename "$submission_basename" .zst).ndjson"
+
+    if [[ -f "$compressed_comment_path" ]]; then
+        rm -fv "$compressed_comment_path"
+    else
+        echo "File not found for deletion: $compressed_comment_path"
+    fi
+
+    if [[ -f "$compressed_submission_path" ]]; then
+        rm -fv "$compressed_submission_path"
+    else
+        echo "File not found for deletion: $compressed_submission_path"
+    fi
+
+    if [[ -f "$released_comment_path" ]]; then
+        rm -fv "$released_comment_path"
+    else
+        echo "File not found for deletion: $released_comment_path"
+    fi
+
+    if [[ -f "$released_submission_path" ]]; then
+        rm -fv "$released_submission_path"
+    else
+        echo "File not found for deletion: $released_submission_path"
+    fi
+
+    echo "Finished processing $comment_basename and $submission_basename"
 }
 
 
--- data/process.sh
+++ data/process.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Process only the passed files
-comment_file=$1
-submission_file=$2
+SSD_DIR=$1
+comment_file_fullname=$2
+submission_file_fullname=$3
 
-# Decompress and process the comment file
-if [[ -n $comment_file ]]; then
-    BASENAME=$(basename $comment_file .zst)
-    zstd -d -f "$SSD_DIR/compressed/$comment_file" --long=31 -o "$SSD_DIR/released/$BASENAME.ndjson"
+# Process the comment file
+if [[ $comment_file_fullname == *.zst ]]; then
+    BASENAME=$(basename $comment_file_fullname .zst)
+    zstd -d -f "$SSD_DIR/compressed/$comment_file_fullname" --long=31 -o "$SSD_DIR/released/$BASENAME.ndjson"
     python3 process_comments.py "$SSD_DIR/released/$BASENAME.ndjson" "$SSD_DIR/processed/$BASENAME.csv"
 fi
 
-# Decompress and process the submission file
-if [[ -n $submission_file ]]; then
-    BASENAME=$(basename $submission_file .zst)
-    zstd -d -f "$SSD_DIR/compressed/$submission_file" --long=31 -o "$SSD_DIR/released/$BASENAME.ndjson"
+# Process the submission file
+if [[ $submission_file_fullname == *.zst ]]; then
+    BASENAME=$(basename $submission_file_fullname .zst)
+    zstd -d -f "$SSD_DIR/compressed/$submission_file_fullname" --long=31 -o "$SSD_DIR/released/$BASENAME.ndjson"
     python3 process_submissions.py "$SSD_DIR/released/$BASENAME.ndjson" "$SSD_DIR/processed/$BASENAME.csv"
 fi
